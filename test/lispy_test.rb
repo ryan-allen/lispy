eb295a1e8eda8be42d700561514a98293af28175
Evan Light <evan.light@tripledogdare.net>
2011-05-20 10:12:27 -0400

    Oops tests were broken but not any more.

--- test/lispy_test.rb
+++ test/lispy_test.rb
@@ -68,44 +68,45 @@ class LispyTest < Test::Unit::TestCase
   end
 
   begin
-    class OptInParsing
+    class OptOutParsing
       extend Lispy
-      acts_lispy :only => [:Foo]
+      acts_lispy :except => [:blech]
 
       Foo "bar" do
         blech
       end
     end
   rescue 
-    @@opt_in_parsing_error = $!
+    @@opt_out_parsing_error = $!
   end
 
   def test_lispy
-    expected = [__FILE__, [__LINE__.to_s, :setup, {:workers=>30, :connections=>1024}],
-                 [__LINE__.to_s, :http,
+    expected = [__FILE__, ["9", :setup, {:workers=>30, :connections=>1024}],
+                 ["10", :http,
                   {:access_log=>:off},
-                  [[__LINE__.to_s, :server,
+                  [["11", :server,
                     {:listen=>80},
-                    [[__LINE__.to_s, :location, "/", [[__LINE__.to_s, :doc_root, "/var/www/website"]]],
-                     [__LINE__.to_s, :location,
+                    [["12", :location, "/", [["13", :doc_root, "/var/www/website"]]],
+                     ["15", :location,
                       "~ .php$",
-                      [[__LINE__.to_s, :fcgi, {:port=>8877}], [__LINE__.to_s, :script_root, "/var/www/website"]]]]]]]]
+                      [["16", :fcgi, {:port=>8877}], ["17", :script_root, "/var/www/website"]]]]]]]]
 
      assert_equal expected, @@whatever
   end
 
   def test_moar_lispy
-    expected = [__FILE__, [[__LINE__.to_s, :setup, []], [__LINE__.to_s, :setup, [], [[__LINE__.to_s, :lol, []], [__LINE__.to_s, :lol, [], [[__LINE__.to_S, :hi, []], [__LINE__.to_s, :hi, []]]]]]]]
+    expected = [__FILE__, ["28", :setup, []], ["29", :setup, [], [["30", :lol, []], ["31", :lol, [], [["32", :hi, []], ["33", :hi, []]]]]]]
 
     assert_equal expected, @@moar_lispy
   end
 
   def test_conditionally_preserving_procs
     quasi_sexp = @@retain_blocks
-    assert_equal :Scenario, quasi_sexp[1][0]
-    assert_equal "My first awesome scenario", quasi_sexp[1][1]
-    assert_equal :Given, quasi_sexp[1][2][0][0]
-    assert_instance_of Proc, quasi_sexp[1][2][0].last
+    assert_equal :Scenario, quasi_sexp[1][1]
+    assert_equal "My first awesome scenario", quasi_sexp[1][2]
+    require 'awesome_print'
+    assert_equal :Given, quasi_sexp[1][3][0][1]
+    assert_instance_of Proc, quasi_sexp[1][3][0].last
   end
 
   def test_opt_in_parsing
@@ -118,7 +119,7 @@ class LispyTest < Test::Unit::TestCase
       flunk if @@opt_out_parsing_fail
     rescue
       assert_instance_of NoMethodError, @@opt_out_parsing_error
-      assert_match "bar", @@opt_out_parsing_error.message
+      assert_match "blech", @@opt_out_parsing_error.message
     end
   end
 end
